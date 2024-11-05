c11e8e5ac4b788b8310d7296e568fcd98222d716
IounOm <61118226+IounOm@users.noreply.github.com>
2024-11-05 16:10:05 +0700

    fix: delete log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,11 @@
 import type { AppProps } from 'next/app'
 import { MapProvider } from '../providers/map-provider'
-// import type { LayoutProps } from '@vercel/examples-ui/layout'
-
-// import { getLayout, Button } from '@vercel/examples-ui'
 
 import '@vercel/examples-ui/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
-  // const { isLoaded } = useMap();
-
-  // if (!isLoaded) return <p>Loading...</p>;
-  // const Layout = getLayout<LayoutProps>(Component)
 
   return (
-    // <Layout
-    //   title="mint-nft"
-    //   path="solutions/mint-nft"
-    //   description="How to mint an NFT"
-    // >
-    //   <Component {...pageProps} />
-    // </Layout>
     <main>
       <MapProvider>
         <Component {...pageProps} />
